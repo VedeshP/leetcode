1b93887faf7c1966bf75b4bad9f24c2411e3b715
VedeshP <vedeshskp@gmail.com>
2024-10-16 23:37:04 +0530

    question 20

---
+++ 20_valid_parentheses.cpp
@@ -0,0 +1,41 @@
+#include <stack>
+#include <cstring>
+#include <string>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+
+        int len = s.length();
+
+        for (int i = 0; s[i] != '\0'; i++)
+        {
+            char ch = s[i];
+
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                stack.push(ch);
+            }
+
+            else if (ch == ')' || ch == '}' || ch == ']')
+            {
+                if (stack.empty() || !isMatchingPair(stack.top(), ch))
+                {
+                    return false;
+                }
+                stack.pop();
+
+            }
+            
+        }
+
+        return stack.empty();
+    }
+
+    bool isMatchingPair(char left, char right)
+    {
+        return (left == '(' && right == ')') ||
+            (left == '{' && right == '}') ||
+            (left == '[' && right == ']');
+    }
+};
