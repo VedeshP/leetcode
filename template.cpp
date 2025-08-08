2d1b682799900bacf8e73223cb0b8b93aef9c64b
VedeshP <vedeshskp@gmail.com>
2025-08-08 09:19:13 +0530

    complete all

--- template.cpp
+++ template.cpp
@@ -14,3 +14,35 @@
 
 // #define fr(i, a, b) for (ll i = a; i < b; i++)
 // #define _fr(i, a, b) for (ll i = a; i > b; i--)
+
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <utility>
+// #include <algorithm>
+// #include <functional>
+// #include <queue> // also has priority queue
+// #include <deque>
+// #include <iomanip> // required for setprecision
+// #include <stack>
+// #include <numeric>
+// #include <string>
+// #include <limits.h>
+// #include <cmath>
+// #include <cctype>
+// #include <map>
+// #include <set>
+
+// using namespace std;
+
+// int gcd(int a,int b)
+// {
+//     if(b == 0) return a;
+//     else return gcd(b, a % b);
+// }
+
+// int lcm(int a,int b)
+// {
+//     return a * b / gcd(a, b);
+// }
